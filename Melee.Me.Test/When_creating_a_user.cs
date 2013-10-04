0c9a4a4c08fe00095e141bed827676c125a5552a
Trevor <trevor@l9digitalstrategies.com>
2013-10-04 15:13:41 -0400

    changed syle of profile page, made it more like this template http://wrapbootstrap.com/preview/WB00U99JJ

--- Melee.Me.Test/When_creating_a_user.cs
+++ Melee.Me.Test/When_creating_a_user.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToTwitter;
 using Melee.Me.Infrastructure.Repository;
 using Moq;
 using Xunit;
@@ -12,6 +13,25 @@ namespace Melee.Me.Test
 {
     public class When_creating_a_user
     {
+        private Mock<ITwitterExecute> execMock;
+
+        TwitterContext InitTwitterContextWithPostToTwitter<TEntity>(string response)
+        {
+            var authMock = new Mock<ITwitterAuthorizer>();
+            execMock = new Mock<ITwitterExecute>();
+            execMock.SetupGet(exec => exec.AuthorizedClient).Returns(authMock.Object);
+            execMock.Setup(
+                exec => exec.PostToTwitter(
+                    It.IsAny<string>(),
+                    It.IsAny<Dictionary<string, string>>(),
+                    It.IsAny<Func<string, TEntity>>()))
+                    .Returns(response);
+
+            var ctx = new TwitterContext(execMock.Object);
+            return ctx;
+        }
+
+
         [Fact]
         public void the_repository_save_should_be_called()
         {
