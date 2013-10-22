8f36f3c29da0f3ee1255d4115c5b141e42f80dff
Trevor <trevor@l9digitalstrategies.com>
2013-10-22 15:30:08 -0400

    Google connection

--- Melee.Me/Controllers/ConnectionsController.cs
+++ Melee.Me/Controllers/ConnectionsController.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using System.Linq;
 using Facebook;
@@ -8,15 +12,131 @@ using Melee.Me.Infrastructure;
 using Melee.Me.Infrastructure.Repository;
 using Melee.Me.Models;
 
+using Microsoft.Web.WebPages.OAuth;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Melee.Me.Controllers
 {
     public class ConnectionsController : Controller
     {
         private readonly IConnectionRepository _repository;
+        private Uri FacebookRedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("FacebookCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
+        private Uri GoogleRedirectUri
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(Request.Url);
+                uriBuilder.Query = null;
+                uriBuilder.Fragment = null;
+                uriBuilder.Path = Url.Action("GoogleCallback");
+                return uriBuilder.Uri;
+            }
+        }
+
 
         public ConnectionsController()
         {
             _repository = new ConnectionRepository();
+        }
+
+        public ActionResult Google()
+        {
+            var uri = GetServiceLoginUrl(GoogleRedirectUri);
+
+            return Redirect(uri.AbsoluteUri);
+        }
+
+        public ActionResult GoogleCallback(string code)
+        {
+            var meleeUser = (UserModel)Session["challenger"];
+            var accessToken = QueryAccessToken(GoogleRedirectUri, code);
+
+            if (Session["challenger"] != null)
+            {
+                meleeUser = (UserModel)Session["challenger"];
+                meleeUser.Connections.Add(_repository.Add(meleeUser.UserId, "Google", accessToken));
+                Session["challenger"] = meleeUser;
+            }
+
+            return RedirectToAction("MyProfile", "Home", meleeUser);
+
+        }
+
+        private Uri GetServiceLoginUrl(Uri returnUrl)
+        {
+            const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/auth";
+            var profileScope = "https://www.googleapis.com/auth/userinfo#profile";
+            var emailScope = "https://www.googleapis.com/auth/userinfo#email";
+            var scope = profileScope + " " + emailScope;
+
+            return BuildUri(authorizationEndpoint, new NameValueCollection
+                {
+                    { "response_type", "code" },
+                    { "client_id", "894864914110-vv4ahuco0e3dm97njeb752hdc67k0q2f.apps.googleusercontent.com" },
+                    { "scope", "email profile"},
+                    { "redirect_uri", returnUrl.AbsoluteUri },
+                    { "access_type", "offline"},
+                    { "state", "" },
+                });
+        }
+
+        private string QueryAccessToken(Uri returnUrl, string authorizationCode)
+        {
+            string TokenEndpoint = "https://accounts.google.com/o/oauth2/token";
+            var postData = HttpUtility.ParseQueryString(string.Empty);
+            postData.Add(new NameValueCollection
+                {
+                    { "grant_type", "authorization_code" },
+                    { "code", authorizationCode },
+                    { "client_id", "894864914110-vv4ahuco0e3dm97njeb752hdc67k0q2f.apps.googleusercontent.com" },
+                    { "client_secret", "rDMDGAWoYoiWmE9gcYLweKRQ" },
+                    { "redirect_uri", returnUrl.GetLeftPart(UriPartial.Path) },
+                });
+
+            var webRequest = (HttpWebRequest)WebRequest.Create(TokenEndpoint);
+
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            using (var s = webRequest.GetRequestStream())
+            using (var sw = new StreamWriter(s))
+                sw.Write(postData.ToString());
+
+            using (var webResponse = webRequest.GetResponse())
+            {
+                var responseStream = webResponse.GetResponseStream();
+                if (responseStream == null)
+                    return null;
+
+                using (var reader = new StreamReader(responseStream))
+                {
+                    var response = reader.ReadToEnd();
+                    var json = JObject.Parse(response);
+                    var accessToken = json.Value<string>("access_token");
+                    return accessToken;
+                }
+            }
+        }
+
+
+        private static Uri BuildUri(string baseUri, NameValueCollection queryParameters)
+        {
+            var q = HttpUtility.ParseQueryString(string.Empty);
+            q.Add(queryParameters);
+            var builder = new UriBuilder(baseUri) { Query = q.ToString() };
+            return builder.Uri;
         }
 
 
@@ -36,10 +156,9 @@ namespace Melee.Me.Controllers
                     {
                         client_id = facebookKey,
                         client_secret = facebookSecret,
-                        redirect_uri = RedirectUri.AbsoluteUri,
+                        redirect_uri = FacebookRedirectUri.AbsoluteUri,
                         response_type = "code",
                         scope = "email,user_status, user_photos, read_stream, read_insights, user_online_presence"
-                        // Add other permissions as needed
                     });
 
                 return Redirect(loginUrl.AbsoluteUri);
@@ -54,17 +173,6 @@ namespace Melee.Me.Controllers
             return View("../Home/MyProfile", meleeUser);
         }
 
-        private Uri RedirectUri
-        {
-            get
-            {
-                var uriBuilder = new UriBuilder(Request.Url);
-                uriBuilder.Query = null;
-                uriBuilder.Fragment = null;
-                uriBuilder.Path = Url.Action("FacebookCallback");
-                return uriBuilder.Uri;
-            }
-        }
 
         public ActionResult FacebookCallback(string code)
         {
@@ -78,7 +186,7 @@ namespace Melee.Me.Controllers
             {
                 client_id = facebookKey,
                 client_secret = facebookSecret,
-                redirect_uri = RedirectUri.AbsoluteUri,
+                redirect_uri = FacebookRedirectUri.AbsoluteUri,
                 code = code
             });
 
---
+++ Melee.Me/Infrastructure/Connection/GoogleConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Facebook;
+using Melee.Me.Models;
+
+namespace Melee.Me.Infrastructure.Connection
+{
+    public class GoogleConnection : IConnection
+    {
+        public string AccessToken { get; set; }
+
+        public double GetScore(UserModel meleeUser)
+        {
+            AccessToken = meleeUser.Connections.Single(x => x.ConnectionName == "Google").AccessToken;
+            double score = 0.00;
+
+            return score;
+        }
+
+    }
+}
