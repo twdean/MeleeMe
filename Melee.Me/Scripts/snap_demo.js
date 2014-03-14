37793e6f3273643a7a92610a21ff46036ab3ba54
Karim Awad <karim@bigtimedesign.ca>
2014-03-14 16:24:05 -0400

    Update to js file to add "right"

--- Melee.Me/Scripts/snap_demo.js
+++ Melee.Me/Scripts/snap_demo.js
@@ -9,6 +9,9 @@ var addEvent = function addEvent(element, eventName, func) {
 addEvent(document.getElementById('open-left'), 'click', function(){
 	snapper.open('left');
 });
+addEvent(document.getElementById('open-right'), 'click', function(){
+	snapper.open('right');
+});
 
 /* Prevent Safari opening links when viewing as a Mobile App */
 (function (a, b, c) {
